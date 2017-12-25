327705654c031e4ae6520b65e691331efc0880fc
User <User@DESKTOP-NLC5870>
2017-12-25 23:49:57 +0300

    Revert "25/12/2017/23:48"
    This reverts commit 233f785b680050bd315cef668ad68aaa3931a657.

--- electronic_queue/src/electronic_queue/Electronic_queue.java
+++ electronic_queue/src/electronic_queue/Electronic_queue.java
@@ -4,12 +4,7 @@
  * and open the template in the editor.
  */
 package electronic_queue;
-import Control.DAO.Audio;
 import electronic_queue.Ui.*;
-import Control.DAO.*;
-import Control.tableModel.AudioTable;
-import java.sql.SQLException;
-import java.util.ArrayList;
 /**
  *
  * @author User
@@ -19,33 +14,10 @@ public class Electronic_queue {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Helloy World");
-     //   Terminal terminal=new Terminal("j");
-     try{
-           System.out.println("Helloy World");
-           Audio a2=new Audio(2,"ttt","jnc");
-           
-     AudioDAO a=new AudioDAO();
-    // a2.setId(2);
-           
-   //a2.setUrl("hef");
-     //a.Add(a2);
-  //  a.update(a2);
-    //a2=a.select(a2);
-    ArrayList<Audio> p=a.select();
-    for( Audio t:p ){
-      System.out.println(t.getUrl());
-     }
-    
-    }
-    // catch(SQLException e){
-   //  System.out.print(e.getMessage());}
-     catch(Exception e){
-     System.out.print(e.getMessage());}
-    // AudioTable aaa=new AudioTable(a.select());
-     
-       //NewJFrame nj=new New JFrame();
+        Terminal terminal=new Terminal("j");
+       //NewJFrame nj=new NewJFrame();
       // terminal.setVisible(true);
     }
     
