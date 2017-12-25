0f60579d2f45a7b0bdc6b6ae98f3b9845bc1ab08
User <User@DESKTOP-NLC5870>
2017-12-25 16:25:47 +0300

    test

--- electronic_queue/src/electronic_queue/Electronic_queue.java
+++ electronic_queue/src/electronic_queue/Electronic_queue.java
@@ -15,7 +15,7 @@ public class Electronic_queue {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("wdwedw");
     }
     
 }
