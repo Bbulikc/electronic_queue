8de0946c74eaca5063844804ffa4d744c8e54da3
Eugene Maltsev <Eugene Maltsev@ZVERDVD-D277RPU>
2017-12-25 17:47:13 +0300

    add

---
+++ electronic_queue/src/Control/USER.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package electronic_queue.Control;
+
+/**
+ *
+ * @author User
+ */
+public class USER {
+    private int id=-1;
+    private String login=null;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+    
+}
